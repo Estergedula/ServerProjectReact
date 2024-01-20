b7ca738067ea02a4cd150aecc833da9029dbad50
shira-l <michalla37@gmail.com>
2024-01-21 01:20:07 +0200

    jhgfds

--- web/src/components/Posts.jsx
+++ web/src/components/Posts.jsx
@@ -1,28 +1,86 @@
-import React, { useContext, useEffect,useState } from "react";
+import React, { useContext, useEffect, useState, useRef, useReducer } from "react";
 import { UserContext } from '../App'
 
 
 const Posts = () => {
-    const [userPosts, setUserPosts] = useState([])
     const { user } = useContext(UserContext);
+    const nextPostId = useRef(0)
+    let initialPosts = []
+
     useEffect(
         () => {
             if (user == null) {
                 navigate('/login')
             }
-            fetch(`http://localhost:3000/posts/?userId=${user.id}`)
-                .then(response => response.json())
-                .then(data => {
-                    setUserPosts(data)
-                })
+            getData()
+            getNextId();
         }, [])
+
+
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case "COMPLETE":
+                return state.map((post) => {
+                    if (post.id === action.id) {
+                        return { ...post, completed: !post.completed };
+                    } else {
+                        return post;
+                    }
+                });
+            case "START":
+                return action.data;
+            case "DELETE":
+                return state.filter(post => post.id != action.id)
+            default:
+                return state;
+        }
+    }
+
+    const getData = () => {
+        fetch(`http://localhost:3000/posts/?userId=${user.id}`)
+            .then(response => response.json())
+            .then(data => {
+                return data.map(post => { return { ...post, update: false } })
+            })
+            .then(initialPosts => { dispatch({ type: "START", data: initialPosts }) })
+
+    }
+    const [userPosts, dispatch] = useReducer(reducer, initialPosts);
+
+
+
+    const getNextId = () => {
+        fetch(`http://localhost:3000/ContinuousNumber/postsId`)
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                nextPostId.current = data.value + 1;
+            })
+    }
+
+
+    const addpost = () => {
+
+    }
+
+    const deletePost = async (taskId) => {
+        await fetch(`http://localhost:3000/posts/${taskId}`, {
+            method: 'DELETE',
+        })
+        dispatch({ type: "DELETE", id: taskId });
+    }
+
+
     return (
         <>
-        <div> {userPosts.map((post) => {
-                return <div className="posts"><span>Post {post.id}</span>
-                    <span>{post.title}</span></div>
+            <div> {userPosts.map((post) => {
+                return <div className="posts" key={post.id}><span>Post {post.id}</span>
+                    <span className="postTitle">{post.title}</span><span className="btnSpan">
+                        <button className="delete" onClick={() => deletePost(post.id)}></button>
+                        <button className="update" onClick={() => updatePost(post.id)}></button></span></div>
             })}</div>
-           
+
         </>
     )
 }
--- web/src/components/Todos.jsx
+++ web/src/components/Todos.jsx
@@ -1,24 +1,83 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useRef, useEffect, useState, useContext, useReducer } from "react";
 import { UserContext } from '../App'
-
+import { useForm } from "react-hook-form";
 
 const Todos = () => {
-    const [userTodos, setUserTodos] = useState([])
     const { user } = useContext(UserContext);
+    const nextTodoId = useRef(0)
+    const {
+        register,
+        handleSubmit,
+        reset,
+    } = useForm();
+    let initialTodos = []
+
     useEffect(
         () => {
             if (user == null) {
                 navigate('/login')
             }
-            fetch(`http://localhost:3000/todos/?userId=${user.id}`)
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data)
-                    setUserTodos(data)
-                })
+            getData()
+            getNextId();
         }, [])
 
-    const handleCheck = (event, taskId) => {
+
+    const reducer = (state, action) => {
+        switch (action.type) {
+            case "COMPLETE":
+                return state.map((todo) => {
+                    if (todo.id === action.id) {
+                        return { ...todo, completed: !todo.completed };
+                    } else {
+                        return todo;
+                    }
+                });
+            case "START":
+                return action.data;
+            case "DELETE":
+                return state.filter(todo => todo.id != action.id)
+            case "DISPLAY":
+                return state.map((todo) => {
+                    if (todo.id === action.id) {
+                        return { ...todo, update: !todo.update };
+                    } else {
+                        return todo;
+                    }
+                });
+            default:
+                return state;
+        }
+    }
+
+    const getData = () => {
+        fetch(`http://localhost:3000/todos/?userId=${user.id}`)
+            .then(response => response.json())
+            .then(data => {
+                return data.map(todo => { return { ...todo, update: false } })
+            })
+            .then(initialTodos => { dispatch({ type: "START", data: initialTodos }) })
+
+    }
+    const [userTodos, dispatch] = useReducer(reducer, initialTodos);
+
+
+
+    const getNextId = () => {
+        fetch(`http://localhost:3000/ContinuousNumber/todosId`)
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                nextTodoId.current = data.value + 1;
+            })
+    }
+
+
+    const addTodo = () => {
+
+    }
+
+    const handleCheckBox = (event, taskId) => {
         fetch(`http://localhost:3000/todos/${taskId}`, {
             method: 'PATCH',
             body: JSON.stringify({
@@ -30,38 +89,44 @@ const Todos = () => {
             },
         })
             .then((response) => response.json())
-            .then(data => changeTodoComplete(data))
+            .then(data => dispatch({ type: "COMPLETE", id: data.id }))
     }
+
 
     const deleteTodo = async (taskId) => {
         await fetch(`http://localhost:3000/todos/${taskId}`, {
             method: 'DELETE',
         })
-        deleteTodoFromState(taskId)
+        dispatch({ type: "DELETE", id: taskId });
     }
 
+    const updateTodo = (todoId) => {
 
-    const deleteTodoFromState = (taskId) => {
-        let updateUserTodos = userTodos.filter(todo => todo.id != taskId)
-        setUserTodos(updateUserTodos)
     }
-    const changeTodoComplete = (taskId) => {
-        let updateUserTodos = userTodos.map(todo => {
-            if (todo.id == taskId) {
-                return { ...todo, complete: !todo.complete }
-            }
-            return todo
-        })
+
+    const onSubmit = (data) => {
+       if(data.key=="add"){
+        console.log(data)
+       }
+       else{
+        console.log()
+       }
     }
     return (
         <>
+            <div id="operations"><button onClick={addTodo}>+</button></div>
             {userTodos.map((todo) => {
-                return <div className="tasks"><span className="idSpan">task Id:{todo.id}</span>
+                return <><div className="tasks" key={todo.id}><span className="idSpan">task Id:{todo.id}</span>
                     <span>{todo.title}</span>
-                    <span className="btnSpan"> <input type="checkbox" checked={todo.completed} onChange={(event) => handleCheck(event, todo.id)} />
+                    <span className="btnSpan"> <input type="checkbox" checked={todo.completed} onChange={(event) => handleCheckBox(event, todo.id)} />
                         <button className="delete" onClick={() => deleteTodo(todo.id)}></button>
-                        <button className="update" onClick={() => updateTodo(todo.id)}></button></span>
+                        <button className="update" onClick={() => { dispatch({ type: "DISPLAY", id: todo.id }); }}></button></span>
                 </div>
+                    {todo.update && <form onSubmit={handleSubmit( onSubmit)}>
+                        <input type="text" placeholder="title" {...register(`${todo.id}`)} /><br />
+                        <button type="submit">Update</button>
+                    </form>}
+                </>
             })}
         </>
     )
